49a953bcb61e0b9f65d8671781fca8af580d69a0
Abhishek Kumar <dev.abhishekkr@gmail.com>
2024-10-26 14:25:38 +0530

    Time: 0 ms (100%), Space: 11.9 MB (8.21%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -4,10 +4,10 @@ public:
         if(i == n-1) return triangle[n-1][j];
     
     if(dp[i][j] != -1) return dp[i][j];
-    int d = triangle[i][j] + solve(i+1, j, triangle, n, dp);
-    int dg = triangle[i][j] + solve(i+1, j+1, triangle, n, dp);
+    int down = triangle[i][j] + solve(i+1, j, triangle, n, dp);
+    int diag = triangle[i][j] + solve(i+1, j+1, triangle, n, dp);
 
-    return dp[i][j] = min(d, dg);
+    return dp[i][j] = min(down, diag);
 }
     int minimumTotal(vector<vector<int>>& triangle) {
         int n =triangle.size();
